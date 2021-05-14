4a5f356f982039957311a0acd120db9752956327
KirkB1693 <kirkblomstrom@prodigy.net>
2021-05-14 14:32:35 -0700

    Animations work properly and updated based on download status, still need to implement custom notifications.

--- app/src/main/java/com/example/loadapp/LoadingButton.kt
+++ app/src/main/java/com/example/loadapp/LoadingButton.kt
@@ -1,40 +1,46 @@
 package com.example.loadapp
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
 import android.animation.ValueAnimator
 import android.content.Context
-import android.content.res.Resources
 import android.graphics.*
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
+import android.view.animation.DecelerateInterpolator
+import androidx.core.content.withStyledAttributes
+import timber.log.Timber
+import kotlin.math.roundToInt
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
     private var widthSize = 0
     private var heightSize = 0
 
-    // Set up paints for canvas drawing.
-    private val backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = ContextCompat.getColor(context, R.color.colorPrimary)
-    }
-    private val textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = ContextCompat.getColor(context, R.color.white)
-        // Set the number text size to be 24sp.
-        // Translate 24sp
-        textSize = Math.round(24f * resources.displayMetrics.scaledDensity).toFloat()
-    }
-
     // Allocate objects needed for canvas drawing here.
-    private val backgroundRect = RectF();
+    private val backgroundRect = RectF()
+    private val ovalLocation = RectF()
 
     // Initialize drawing measurements.
-    private val cornerRadius = Math.round(7f * resources.displayMetrics.density);
+    private val cornerRadius = (7f * resources.displayMetrics.density).roundToInt();
 
-    private val valueAnimator = ValueAnimator.ofFloat(0f, -(width.toFloat()))
+    private val valueAnimatorForWidth = ValueAnimator()
+    private val valueAnimatorForAngle = ValueAnimator()
+
+    private var animatedWidth: Float = 0f
+    private var animatedAngle: Float = 0f
+    private var animatedStartWidth: Float = 0f
+    private var animatedStartAngle: Float = 0f
+    private var oldPercentage: Float = 0f
+
+    private var displayedText = resources.getString(R.string.button_name)
+
+    private var loadingPercentage = 0f
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
         when (new) {
@@ -45,58 +51,196 @@ class LoadingButton @JvmOverloads constructor(
     }
 
     private fun stopButtonAnimation() {
-        TODO("Not yet implemented")
+        displayedText = resources.getString(R.string.button_name)
+        invalidate()
     }
 
     private fun continueButtonAnimation() {
-        TODO("Not yet implemented")
+        displayedText = resources.getString(R.string.button_loading)
+        // set a small percentage just to show user something is happening even if current download percentage is less
+        setLoadingPercentage(.03f)
+    }
+
+    private fun animateCircle(progress: Float) {
+        valueAnimatorForAngle.apply {
+            setFloatValues(animatedStartAngle, 360f)
+            interpolator = DecelerateInterpolator()
+        }
+
+        valueAnimatorForAngle.addUpdateListener {
+            val interpolation = it.animatedFraction
+            animatedAngle = 360f * interpolation * progress
+            invalidate()
+        }
+
+        valueAnimatorForAngle.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                animatedStartAngle = 360f * progress
+            }
+        })
+
+    }
+
+    private fun animateLoading(progress: Float) {
+        valueAnimatorForWidth.apply {
+            setFloatValues(animatedStartWidth, width.toFloat())
+            interpolator = DecelerateInterpolator()
+            invalidate()
+        }
+
+        valueAnimatorForWidth.addUpdateListener {
+            val interpolation = it.animatedFraction
+            animatedWidth = width * interpolation * progress
+        }
+
+        valueAnimatorForWidth.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                animatedStartWidth = width * progress
+            }
+        })
+
     }
 
     private fun startButtonAnimation() {
-        TODO("Not yet implemented")
+        displayedText = resources.getString(R.string.button_loading)
+        animatedStartWidth = 0f
+        animatedStartAngle = 0f
+        animatedWidth = 0f
+        animatedAngle = 0f
+        loadingPercentage = 0f
+        oldPercentage = 0f
+        invalidate()
     }
 
-    private val displayedText = resources.getString(R.string.button_name)
-
-    private val path = Path()
+    private var buttonBackgroundColor = 0
+    private var buttonLoadingColor = 0
+    private var buttonLoadingCircleColor = 0
+    private var buttonTextColor = 0
 
     init {
         isClickable = true
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            buttonBackgroundColor = getColor(R.styleable.LoadingButton_buttonColor, 0)
+            buttonLoadingColor = getColor(R.styleable.LoadingButton_loadingColor, 0)
+            buttonLoadingCircleColor = getColor(R.styleable.LoadingButton_circleColor, 0)
+            buttonTextColor = getColor(R.styleable.LoadingButton_buttonTextColor, 0)
+        }
+        buttonState = ButtonState.Completed
     }
 
+    // Set up paints for canvas drawing.
+    private val backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = buttonBackgroundColor
+    }
+    private val textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = buttonTextColor
+        // Set the number text size to be 24sp.
+        // Translate 24sp
+        textSize = ((24f * resources.displayMetrics.scaledDensity).roundToInt()).toFloat()
+    }
+    private val loadingPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = buttonLoadingColor
+    }
+    private val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = buttonLoadingCircleColor
+    }
 
     override fun performClick(): Boolean {
-        if (super.performClick()) return true
-        invalidate()
-        return true
+        return super.performClick()
     }
 
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        // Grab canvas dimensions.
-        val canvasWidth = width
-        val canvasHeight = height
 
+        if (buttonState == ButtonState.Completed) {
+            drawBaseButton(canvas)
+            Timber.i("Drawing base button")
+        } else {
+            val percentForDisplay = (loadingPercentage * 100).toInt()
+            Timber.i("Button animation, percentage : $percentForDisplay")
+            drawLoadingBackground(canvas)
+            drawLoadingAnimation(canvas)
+            // draw text after animation so text stays visible
+            drawText(canvas)
+            drawLoadingCircle(canvas)
+        }
+
+    }
+
+    private fun drawLoadingBackground(canvas: Canvas) {
+        // Draw the background.
+        backgroundRect.set(0f, 0f, width.toFloat(), height.toFloat())
+        canvas.drawRoundRect(
+            backgroundRect, cornerRadius.toFloat(),
+            cornerRadius.toFloat(), backgroundPaint
+        )
+    }
+
+    private fun drawLoadingAnimation(canvas: Canvas) {
+        canvas.drawRoundRect(0f, 0f, animatedStartWidth, height.toFloat(),
+            cornerRadius.toFloat(), cornerRadius.toFloat(), loadingPaint)
+        if (animatedStartWidth >= cornerRadius.toFloat()) {
+            canvas.drawRoundRect(
+                animatedStartWidth - cornerRadius.toFloat(), 0f, animatedWidth, height.toFloat(),
+                cornerRadius.toFloat(), cornerRadius.toFloat(), loadingPaint
+            )
+        } else {
+            canvas.drawRoundRect(
+                animatedStartWidth , 0f, animatedWidth, height.toFloat(),
+                cornerRadius.toFloat(), cornerRadius.toFloat(), loadingPaint
+            )
+        }
+
+    }
+
+    private fun drawText(canvas: Canvas) {
         // Calculate horizontal center.
-        val centerX = canvasWidth * 0.5f
+        val centerX = width * 0.5f
 
         // Calculate the baseline for the text
-        val baselineY = Math.round(canvasHeight * 0.6f);
-
-        // Draw the background.
-        backgroundRect.set(0f, 0f, canvasWidth.toFloat(), canvasHeight.toFloat());
-        canvas.drawRoundRect(backgroundRect, cornerRadius.toFloat(),
-            cornerRadius.toFloat(), backgroundPaint);
-
-        // Draw text.
+        val baselineY = (height * 0.6f).roundToInt()
 
         // Measure the width of text to display.
-        val textWidth = textPaint.measureText(displayedText);
+        val textWidth = textPaint.measureText(displayedText)
         // Figure out an x-coordinate that will center the text in the canvas.
-        val textX = Math.round(centerX - textWidth * 0.5f);
+        val textX = (centerX - textWidth * 0.5f).roundToInt()
         // Draw.
-        canvas.drawText(displayedText, textX.toFloat(), baselineY.toFloat(), textPaint);
+        canvas.drawText(displayedText, textX.toFloat(), baselineY.toFloat(), textPaint)
+
+        ovalLocation.set((centerX + textWidth * 0.5f + 4), (heightSize * 0.2).toFloat(), (centerX + textWidth * 0.5f + 4)+(heightSize * 0.6).toFloat(), (heightSize * 0.8).toFloat())
+    }
+
+    private fun drawLoadingCircle(canvas: Canvas) {
+        //draw Arc
+        canvas.drawArc(
+            ovalLocation,
+            0f,
+            animatedStartAngle,
+            true,
+            circlePaint
+        )
+        //draw Arc
+        canvas.drawArc(
+            ovalLocation,
+            animatedStartAngle,
+            animatedAngle-animatedStartAngle,
+            true,
+            circlePaint
+        )
+    }
+
+    private fun drawBaseButton(canvas: Canvas) {
+        // Draw the background.
+        backgroundRect.set(0f, 0f, width.toFloat(), height.toFloat())
+        canvas.drawRoundRect(
+            backgroundRect, cornerRadius.toFloat(),
+            cornerRadius.toFloat(), backgroundPaint
+        )
+
+        drawText(canvas)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -110,6 +254,38 @@ class LoadingButton @JvmOverloads constructor(
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
+    }
+
+    fun setCustomButtonState(state: ButtonState) {
+        buttonState = state
+    }
+
+    fun setLoadingPercentage(percentage: Float) {
+        var adjustedPercentage = 0.04f
+        if (percentage > 0.04f) {
+            adjustedPercentage = percentage
+        }
+        setLoadingPercentage(adjustedPercentage, true)
+    }
+
+    fun setLoadingPercentage(percentage: Float, animate: Boolean) {
+
+        if (animate) {
+            if (percentage > oldPercentage) {
+                animateLoading(percentage)
+                animateCircle(percentage)
+                oldPercentage = percentage
+                val set = AnimatorSet()
+                set.playTogether(valueAnimatorForAngle, valueAnimatorForWidth)
+                set.duration = 500L
+                if (!set.isStarted) {
+                    set.start()
+                }
+            }
+        } else {
+            loadingPercentage = percentage
+            postInvalidate()
+        }
     }
 
 }
--- app/src/main/java/com/example/loadapp/MainActivity.kt
+++ app/src/main/java/com/example/loadapp/MainActivity.kt
@@ -7,19 +7,25 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.widget.RadioButton
-import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import com.example.loadapp.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     private var downloadID: Long = 0
 
@@ -33,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(toolbar)
+        Timber.plant(Timber.DebugTree())
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
@@ -40,31 +47,20 @@ class MainActivity : AppCompatActivity() {
         custom_button.setOnClickListener {
             // Get the checked radio button id from radio group
             var id: Int = radioGroup.checkedRadioButtonId
-            if (id != -1) { // If any radio button checked from radio group
+            if (id != -1) {
+                disableLoadingButton()
+                // If any radio button checked from radio group
                 // Get the instance of radio button using id
                 val radio: RadioButton = findViewById(id)
                 when (radio) {
-                    radioButtonGlide -> Toast.makeText(
-                        applicationContext, "On button click :" +
-                                " ${radio.text}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    radioButtonLoadApp -> Toast.makeText(
-                        applicationContext, "On button click :" +
-                                " ${radio.text}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    radioButtonRetrofit -> Toast.makeText(
-                        applicationContext, "On button click :" +
-                                " ${radio.text}",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    radioButtonGlide -> applicationScope.launch {download(GLIDE_URL, GLIDE_SIZE)}
+                    radioButtonLoadApp -> applicationScope.launch {download(LOAD_APP_URL, LOAD_APP_SIZE)}
+                    radioButtonRetrofit -> applicationScope.launch {download(RETROFIT_URL, RETROFIT_SIZE)}
                     else -> Toast.makeText(
                         applicationContext, "Unknown Radio Button Selected",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
-
             } else {
                 // If no radio button checked in this radio group
                 Toast.makeText(
@@ -76,29 +72,179 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun disableLoadingButton() {
+        custom_button.isEnabled = false
+    }
+
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
         }
     }
 
-    private fun download() {
+    private fun download(url: String, bytesTotal: Long) {
+        custom_button.setCustomButtonState(ButtonState.Clicked)
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+
+        var downloading = true
+        runOnUiThread { custom_button.setCustomButtonState(ButtonState.Loading) }
+
+        Timber.i("Download starting...")
+        var downloadProgress: Int
+        var oldDownloadProgress = 0
+        while (downloading) {
+            Thread.sleep(500)
+            val query = DownloadManager.Query()
+
+            val cursor = downloadManager.query(query)
+
+            cursor.moveToFirst()
+            val bytesDownloaded =
+                cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
+
+            downloadProgress = if (bytesTotal != 0L) {
+                ((bytesDownloaded * 100L) / bytesTotal).toInt()
+            } else {
+                100
+            }
+            if (downloadProgress > oldDownloadProgress) {
+                if (downloadProgress > 100) {
+                    downloadProgress = 100
+                }
+                runOnUiThread { custom_button.setLoadingPercentage(downloadProgress / 100f) }
+                oldDownloadProgress = downloadProgress
+            }
+            Timber.i("Download at $downloadProgress percent")
+            downloading = checkDownloadStatus(cursor, downloading)
+            cursor.close()
+        }
+    }
+
+    private fun checkDownloadStatus(
+        cursor: Cursor,
+        downloading: Boolean
+    ): Boolean {
+        var downloading1 = downloading
+        val downloadStatus = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+        if (downloadStatus == DownloadManager.STATUS_SUCCESSFUL ||
+            downloadStatus == DownloadManager.STATUS_FAILED
+        ) {
+            downloading1 = false
+            Thread.sleep(2500)
+            runOnUiThread {
+                enableLoadingButton()
+                custom_button.setCustomButtonState(ButtonState.Completed)
+            }
+        }
+        val statusMessage = getDownloadStatus(downloadStatus)
+        Timber.i("Download status is $statusMessage")
+        val columnReason = cursor.getColumnIndex(DownloadManager.COLUMN_REASON)
+        val reason = cursor.getInt(columnReason)
+
+        when (downloadStatus) {
+            DownloadManager.STATUS_FAILED -> {
+                val failedReason = getFailedReason(reason)
+                Timber.i("FAILED: $failedReason")
+                runOnUiThread {
+                    Toast.makeText(
+                        this,
+                        "FAILED",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+            DownloadManager.STATUS_PAUSED -> {
+                val pausedReason = getPausedReason(reason)
+                Timber.i("PAUSED: $pausedReason")
+            }
+            DownloadManager.STATUS_PENDING -> Timber.i("Pending")
+            DownloadManager.STATUS_RUNNING -> Timber.i("Running")
+            DownloadManager.STATUS_SUCCESSFUL -> {
+                runOnUiThread {
+                    Toast.makeText(
+                        this,
+                        "SUCCESSFUL",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+        return downloading1
+    }
+
+    private fun getPausedReason(reason: Int): String {
+        var pausedReason = ""
+        when (reason) {
+            DownloadManager.PAUSED_QUEUED_FOR_WIFI -> pausedReason =
+                "PAUSED_QUEUED_FOR_WIFI"
+            DownloadManager.PAUSED_UNKNOWN -> pausedReason = "PAUSED_UNKNOWN"
+            DownloadManager.PAUSED_WAITING_FOR_NETWORK -> pausedReason =
+                "PAUSED_WAITING_FOR_NETWORK"
+            DownloadManager.PAUSED_WAITING_TO_RETRY -> pausedReason =
+                "PAUSED_WAITING_TO_RETRY"
+        }
+        return pausedReason
+    }
+
+    private fun getFailedReason(reason: Int): String {
+        var failedReason = ""
+        when (reason) {
+            DownloadManager.ERROR_CANNOT_RESUME -> failedReason = "ERROR_CANNOT_RESUME"
+            DownloadManager.ERROR_DEVICE_NOT_FOUND -> failedReason =
+                "ERROR_DEVICE_NOT_FOUND"
+            DownloadManager.ERROR_FILE_ALREADY_EXISTS -> failedReason =
+                "ERROR_FILE_ALREADY_EXISTS"
+            DownloadManager.ERROR_FILE_ERROR -> failedReason = "ERROR_FILE_ERROR"
+            DownloadManager.ERROR_HTTP_DATA_ERROR -> failedReason =
+                "ERROR_HTTP_DATA_ERROR"
+            DownloadManager.ERROR_INSUFFICIENT_SPACE -> failedReason =
+                "ERROR_INSUFFICIENT_SPACE"
+            DownloadManager.ERROR_TOO_MANY_REDIRECTS -> failedReason =
+                "ERROR_TOO_MANY_REDIRECTS"
+            DownloadManager.ERROR_UNHANDLED_HTTP_CODE -> failedReason =
+                "ERROR_UNHANDLED_HTTP_CODE"
+            DownloadManager.ERROR_UNKNOWN -> failedReason = "ERROR_UNKNOWN"
+        }
+        return failedReason
+    }
+
+    private fun enableLoadingButton() {
+        custom_button.isEnabled = true
+    }
+
+    private fun getDownloadStatus(status: Int): String {
+        var statusMessage = ""
+        when (status) {
+            DownloadManager.STATUS_FAILED -> statusMessage = "Download Failed"
+            DownloadManager.STATUS_PAUSED -> statusMessage = "Download Paused"
+            DownloadManager.STATUS_PENDING -> statusMessage = "Download Pending"
+            DownloadManager.STATUS_RUNNING -> statusMessage = "Download Running"
+            DownloadManager.STATUS_SUCCESSFUL -> statusMessage = "Download Successful"
+        }
+        return statusMessage
     }
 
     companion object {
-        private const val URL =
+        private const val GLIDE_URL =
+            "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+        private const val GLIDE_SIZE = 93218406L
+        private const val LOAD_APP_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val LOAD_APP_SIZE = 147456L
+        private const val RETROFIT_URL =
+            "https://github.com/square/retrofit/archive/refs/heads/master.zip"
+        private const val RETROFIT_SIZE = 5641338L
         private const val CHANNEL_ID = "channelId"
     }
 
